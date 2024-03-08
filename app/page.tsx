081435c5622c26e7e23bc0a3109daa6391b4d937
Feiran Wang <62454493+Brack-Wang@users.noreply.github.com>
2024-03-08 09:22:42 -0600

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -18,13 +18,14 @@ export default function Home() {
         
         {/* TODO: Change the content for weekly events' information below */}
         <h2 className="text-2xl font-bold mb-2">【本周安排】报名中</h2>
-        <p className="mb-4">3/10 周日 1PM - 4PM<br />️小桌Shift制主题聊天 （i人友好）<br />限定报名人数：9人</p>
+        <p className="mb-4">3/10 周日 1PM - 4PM<br />️小桌Shift制主题聊天 （i人友好）<br />限定报名人数：9人，微信限定4人</p>
         <p className="mb-4">320 River Bar<br />📍320 N Dearborn St, Chicago, IL 60654<br />Located in: The Westin Chicago River North</p>
         <a href="https://www.eventbrite.co.nz/e/cfcht-310-1pm-tickets-858290468897?aff=oddtdtcreator" target="_blank" className="inline-block bg-blue-500 text-white p-2 rounded hover:bg-blue-700 transition duration-300 ease-in-out">报名🔗</a>
         <p className="mt-4">本次活动主题投票（此投票不等于报名）<br /><a href="https://forms.gle/3BLyjCJshx5oz3Uv8" className="text-blue-600 hover:underline" target="_blank">点击这里投票</a></p>
         <p className="mt-4">报名成功的各位，饮品统一预订，点单Google Form链接<br /><a href="https://forms.gle/SiBYXj3vpWv8xURL6" className="text-blue-600 hover:underline" target="_blank">点击打开填写</a></p>
         <p className="mt-4 italic">*报名成功后，如无法出席请在eventbrite取消，平台waitlist将自动补位，请各位留意自己的邮箱</p>
         <p className="mt-4 font-bold">那么各位，这周日见！🌞</p>
+
 
       </div>
       {/* Event Details Replacement End */}
