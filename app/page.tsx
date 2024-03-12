523edd95e340d12f73276d368132ca8869e8572e
CQ <115588037+CQ-YIN@users.noreply.github.com>
2024-03-12 13:42:12 -0500

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -18,20 +18,24 @@ export default function Home() {
       <div className="mt-20 my-12 p-4 md:p-6 max-w-xs md:max-w-2xl lg:max-w-4xl w-full bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700 text-center">
         
         {/* TODO: Change the content for weekly events' information below */}
-        <h2>本周安排</h2>
-        <p>报名中</p>
-        <p>3/16 周六 1PM - 2:30PM</p>
-        <p>🔥室内游戏</p>
-        <p>33🔪/人（含税），参与者将到店每人分别向店家付款</p>
-        <p>限定报名人数：20人，Waitlist人数：5人</p>
-        <p>Activate - Chicago (Oak Brook)</p>
-        <p>📍1600 16th St Unit 6, Oak Brook, IL 60523</p>
-        <p>（距离Loop车程大概30分钟，请各位合理安排出行时间）</p>
-        <p>看需求灵活变通：游戏结束后，大家想的话可以一起去看染绿的湖，散散步</p>
-        <p>*报名成功，但未及时到场：店家如要求损失费，请未到场的个人承担全部所需费用</p>
-        <p>*报名成功后，如无法出席请在eventbrite取消。平台waitlist将发送补位邮件，请大家留意自己的邮箱</p>
-        <p>*请大家至少提前一天取消</p>
-        <p>那么各位，这周六见！🌞</p>
+  <h2>芝加哥CFCHT</h2>
+  <h2>【本周安排】报名中</h2>
+  <p>3/16 周六 1PM - 2:30PM</p>
+  <p>🔥室内游戏</p>
+  <p>33🔪/人（含税），参与者将到店每人分别向店家付款</p>
+  <p>限定报名人数：20人，Waitlist人数：5人</p>
+
+  <p>Activate - Chicago (Oak Brook)</p>
+  <p>📍1600 16th St Unit 6, Oak Brook, IL 60523</p>
+  <p>（距离Loop车程大概30分钟，请各位合理安排出行时间）</p>
+  <p></p>
+  <p>看需求灵活变通：游戏结束后，大家想的话可以一起去看染绿的湖，散散步</p>
+
+  <p>链接🔗
+    <button onClick={() => window.location.href = "https://www.eventbrite.com/e/861313109697?aff=oddtdtcreator"}>
+    </button>
+  </p>
+
 
 
       </div>
