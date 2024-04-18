7287cbcd46885a05fec1f948e92062efa929c0a2
CQ <115588037+CQ-YIN@users.noreply.github.com>
2024-04-17 19:49:12 -0500

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -26,9 +26,9 @@ export default function Home() {
 <p className="mb-4">🔥Activate<br />店家票价：$32.24/人, 限定报名人数：21人</p>
 <p className="mb-4">地点：Activate - Chicago (Oak Brook)<br />📍1600 16th St Unit 6, Oak Brook, IL 60523<br />距离loop车程大概25分钟左右，请各位合理安排出行时间</p>
 <a href="https://www.eventbrite.com/e/cfcht420-240pm-activate-tickets-883886537427?aff=oddtdtcreator" target="_blank" className="inline-block bg-blue-500 text-white p-2 rounded hover:bg-blue-700 transition duration-300 ease-in-out">报名🔗</a>
-<p className="mt-4">Zelle:<br />SHENGKUN CUI<br />shengkunrz@hotmail.com</p>
+<p className="mt-4">Zelle:<br />Chloe Yin<br />yin_chloe@icloud.com</p>
 <p className="mt-4">管理员已付好定金，到店后管理员会付完全款。<br />因为之前活动出现过报名后但最后未出现而造成损失的情况，所以咱们这次提前AA门票。</p>
-<p className="mt-4">*请报名当天23:59前向管理员转zelle，并私信管理员小红书@瑶夜的猫 转账zelle名字+Eventbrite报名名字，否则将取消名额</p>
+<p className="mt-4">*请报名当天23:59前向群主转zelle，并私信群主转账zelle名字+Eventbrite报名名字，否则将取消名额</p>
 <p className="mt-4">*如报名后无法出席，请联系群里其他小伙伴，进行转票</p>
 <p className="mt-4 font-bold">那么各位，周六见！🌞</p>
     <p className="mt-4 font-bold">固定报名时间：每周三8PM</p>
