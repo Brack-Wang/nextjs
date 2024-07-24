06a8b3aa60e9aecc83950bb563dcb9f5b1890e9d
Shengkun Cui <91434851+shengkunc@users.noreply.github.com>
2024-07-24 17:35:12 -0500

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -22,9 +22,9 @@ export default function Home() {
       {/* TODO: Change the content for weekly events' information below */}
   {/* Event Details Replacement Start */}
 <h2 className="text-2xl font-bold mb-2">【本周安排】报名中</h2>
-<h2 className="text-2xl font-bold mb-2">7/20 周六 3:00PM - 4:45PM</h2>
-<p className="mb-4">🔥Activate返场啦 <br />限定报名人数：13人</p>
-<p className="mb-4">地点：Activate - Chicago (Oak Brook)<br />📍1600 16th St Unit 6, Oak Brook, IL 60523<br />距离loop车程大概35分钟左右，请各位合理安排出行时间<br />活动后计划一起聚餐</p>
+<h2 className="text-2xl font-bold mb-2">7/27 周六 11:00AM - 10:00PM</h2>
+<p className="mb-4">🔥万众期待的Six Flags! <br />限定报名人数：12人</p>
+<p className="mb-4">地点：Six Flags Great America <br />📍1 Great America Parkway, Gurnee, IL 60031<br />距离loop车程大概1小时左右，请各位合理安排出行时间</p>
 <a href="https://www.eventbrite.com/e/cfcht720-300-pm-activate-tickets-948294182427?aff=oddtdtcreator" target="_blank" className="inline-block bg-blue-500 text-white p-2 rounded hover:bg-blue-700 transition duration-300 ease-in-out">报名🔗</a>
 <p className="mt-4">管理员已付好定金，到店后管理员会付完全款。<br />因为之前活动出现过报名后但最后未出现而造成损失的情况，所以咱们提前AA门票。</p>
 <p className="mt-4">*请在eventbrite报名后，当天23:59前私信管理员@Timaximum （付款管理员小红书昵称）Eventbrite报名名字，管理员将告知zelle转账方式，否则将取消名额</p>
