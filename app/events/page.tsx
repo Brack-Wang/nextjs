b230a5ca3c8c7d66e48a80c962b2f31c184c8f39
TianyouLuo <luotianyou1996@outlook.com>
2024-08-12 19:44:23 -0500

    Update page.tsx

--- app/events/page.tsx
+++ app/events/page.tsx
@@ -12,7 +12,12 @@ import Link from 'next/link';
 
 const eventsData = [
      // Add the events as the following template ahead
-             {
+   {
+    imageUrl: '/0803小桌.jpg', // 替换为图片路径,请注意后缀名".jpg"或者".png"要和你上传的图片相同
+    websiteUrl: 'http://xhslink.com/KSi70S', // 替换为对应的网站链接
+    altText: '8/3 主题性小桌聊天 @Sawada Coffee', // 图片的替代文本
+  },  
+   {
     imageUrl: '/0713小桌.jpg', // 替换为图片路径,请注意后缀名".jpg"或者".png"要和你上传的图片相同
     websiteUrl: 'http://xhslink.com/oxEFiR', // 替换为对应的网站链接
     altText: '7/13 主题性小桌聊天 @Sawada Coffee', // 图片的替代文本
