bbe191a91be7523924677c6bd9b1b0938a3d2bf9
CQ <115588037+CQ-YIN@users.noreply.github.com>
2024-10-02 22:04:34 -0500

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -34,7 +34,7 @@ export default function Home() {
         <p className="mb-4">活动简介：<br />2轮Shift制小桌主题聊天 + Gather + 自由讨论</p>
         <p className="mb-4">共有4个不同主题的聚点，每个聚点4人，每轮45分钟，2轮后轮换其他主题。</p>
         <p className="mb-4">本次话题包括：<br />音乐分享、来芝加哥/美国的理由/想做什么、保暖建议、秋冬必备等</p>
-        <p className="mt-4">*报名成功后，请于当天22:00前通过Zelle支付场地费用，否则名额将被取消</p>
+        <p className="mt-4">*报名成功后，请于当天23:59前通过Zelle支付场地费用，否则名额将被取消</p>
         <p className="mt-4">*如报名后无法出席，请联系群里的其他成员转让票务</p>
         <p className="mt-4">*此次活动为我们自行预约场地的尝试。以往免费场地常出现人数限制或店家不支持活动的情况。本次收取的费用将用于场地预订。若费用不足，可能会更换场地，敬请理解！如有变动，会提前通知所有参与者。</p>
       {/* Event Details Replacement End */}
