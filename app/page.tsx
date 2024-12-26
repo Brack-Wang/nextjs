e8519009f00d867b12a11aee96a3f495248ca36b
CQ <115588037+CQ-YIN@users.noreply.github.com>
2024-12-26 16:00:29 -0600

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -18,46 +18,62 @@ export default function Home() {
         <h2 className="text-2xl font-bold mb-2"> 芝加哥CFCHT </h2>
 
 
+{/* TODO: Change the content for weekly events' information below */}
 {/* Event Details Replacement Start */}
 <h2 className="text-2xl font-bold mb-2">【本周安排】报名中</h2>
-<h2 className="text-2xl font-bold mb-2">12/21 周六 11:30 Gallery Day + 午餐</h2>
-<p className="mb-4">限定人数：6人</p>
-<a href="https://www.eventbrite.co.nz/e/1221-1100-gallery-day-6-tickets-1116789480399" target="_blank" className="inline-block bg-blue-500 text-white p-2 rounded hover:bg-blue-700 transition duration-300 ease-in-out">报名🔗</a>
-
-<p className="mb-4">Agenda：</p>
+<h2 className="text-2xl font-bold mb-2">12/31 周二 6:30 PM - 0:30 AM 🎆跨年</h2>
 <p className="mb-4">
-  11:30 <a href="https://corbettvsdempsey.com/exhibitions/celeste-rapone_2024/" target="_blank" className="text-blue-500 underline">Corbett Vs Dempsey</a><br />
-  📍2156 W Fulton St, Chicago, IL 60612
+  报名
+  <a href="https://www.eventbrite.com/e/cfcht1231-630-tickets-1124633893249?aff=oddtdtcreator" target="_blank" className="inline-block bg-blue-500 text-white p-2 rounded hover:bg-blue-700 transition duration-300 ease-in-out">报名🔗</a>
 </p>
-
-<p className="mb-4">🚗 5mins</p>
-
 <p className="mb-4">
-  12:00 午餐 @Yuzu Sushi & Robata Grill<br />
-  📍1751 W Chicago Ave, Chicago, IL 60622
+  📍 Marquee at Block 37<br />
+  25 W Randolph St, Chicago, IL, 60601<br />
+  （请准时到大堂集合）
 </p>
-
-<p className="mb-4">🚶步行2mins</p>
-
+<h3 className="text-xl font-bold mt-4">🎯 Agenda：</h3>
 <p className="mb-4">
-  1:00 <a href="http://wvvolumes.com/current/" target="_blank" className="text-blue-500 underline">Volume Gallery</a><br />
-  📍1709 W Chicago Ave 2nd floor, Chicago, IL 60622
+  6:30-11 PM 晚餐 + 交换礼物 + 桌游<br />
+  🚗 Navy Pier附近停车 & 散步去看烟花<br />
+  *建议跟管理员车，我们已预订停车位*2。友情提示：自驾的各位请注意提前预约车位，跨年当天可能不好找。
 </p>
 
-<p className="mb-4">同栋/隔壁建筑物内其他画廊展览：</p>
-<p className="mb-2">
-  1. <a href="https://documentspace.com/exhibition/chicago-anneke-eussen-beyond-purpose/" target="_blank" className="text-blue-500 underline">Document</a>
-</p>
-<p className="mb-2">
-  2. <a href="https://westernexhibitions.com/current-exhibition/" target="_blank" className="text-blue-500 underline">Western Exhibitions</a>
-</p>
-<p className="mb-2">
-  3. <a href="https://www.rhoffmangallery.com/exhibitions/these-times" target="_blank" className="text-blue-500 underline">Rhona Hoffman Gallery</a>
+<h3 className="text-xl font-bold mt-4">🎁 交换礼物：</h3>
+<p className="mb-4">
+  * 请准备价值$10~25的小礼物！<br />
+  * 交换方式当天公布，不需要精美包装，但需不看到礼物本身。
 </p>
 
-<p className="mt-4">*如报名后无法出席，请联系群里其他小伙伴，进行转票</p>
+<h3 className="text-xl font-bold mt-4">🍽️ 晚餐AA：</h3>
+<p className="mb-4">
+  外卖餐厅选择：<br />
+  - Central East Asian Cuisine 印象新疆<br />
+  - Taipei Cafe 小台北<br />
+  *请告知管理员 Justin 想点的菜（两间餐馆共选1-2道菜/人）。
+</p>
+
+<h3 className="text-xl font-bold mt-4">🧩桌游：</h3>
+<p className="mb-4">
+  提供游戏：爆炸猫、说书人、你画我猜、卡坦岛<br />
+  * 也欢迎自带桌游。
+</p>
+
+<h3 className="text-xl font-bold mt-4">🚗 公寓附近停车资讯：</h3>
+      <p className="mb-4">
+  - Theatre District Garage: 181 N Dearborn St.<br />
+  - Wabash Randolph Garage: 20 East Randolph St.<br />
+  - Government Center Garage: 81 W Lake St.<br />
+</p>
+<p className="mb-4">
+  * 请在Eventbrite报名后，当天23:59前私信管理员 Justin，同时备注Eventbrite报名名字 & 菜名及其餐厅，否则将取消名额。<br />
+  * 现场有饮料，欢迎带想分享的东西。如有，请提前通知管理员。<br />
+  * 如报名后无法出席，请联系群里其他小伙伴，进行转名额。
+</p>
+
+<p className="mt-4 font-bold">那么各位，跨年夜见！🎆</p>
 <p className="mt-4 font-bold">固定报名时间：每周三8PM</p>
 {/* Event Details Replacement End */}
+
 
 
 
